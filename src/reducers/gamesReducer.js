93fcc627ee260490c4c82ae97799e0860e16aea4
mic-babin <mic.babin@outlook.com>
2020-11-24 05:53:09 -0500

    4.Thunk set up

--- src/reducers/gamesReducer.js
+++ src/reducers/gamesReducer.js
@@ -2,12 +2,18 @@ const initState = {
   popular: [],
   newGames: [],
   upcoming: [],
+  searched: [],
 };
 
 const gamesReducer = (state = initState, action) => {
   switch (action.type) {
     case "FETCH_GAMES":
-      return { ...state };
+      return {
+        ...state,
+        popular: action.payload.popular,
+        upcoming: action.payload.upcoming,
+        newGames: action.payload.newGames,
+      };
     default:
       return { ...state };
   }
