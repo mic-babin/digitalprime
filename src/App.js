6ad5473f8c141c58e60a4593242b6d00883b850c
mic-babin <mic.babin@outlook.com>
2020-11-24 07:14:02 -0500

    9. Global Styles

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
-import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { loadGames } from "./actions/gamesAction";
+import React from "react";
+// Components and pages
+import Home from "./pages/Home";
+import GlobalStyles from "./components/GlobalStyles";
 
 function App() {
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(loadGames());
-  });
   return (
     <div className="App">
-      <h1>Digital Prime </h1>
+      <GlobalStyles />
+      <Home />
     </div>
   );
 }
