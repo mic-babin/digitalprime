6dfc2164ece55dd4ff090bafcbb277365ca61499
mic-babin <mic.babin@outlook.com>
2020-11-24 10:42:55 -0500

    Game Detail  + Styling

--- src/reducers/detailReducer.js
+++ src/reducers/detailReducer.js
@@ -1,4 +1,8 @@
-const initialState = { game: {}, screen: {} };
+const initialState = {
+  game: { platforms: [] },
+  screen: { results: [] },
+  isLoading: true,
+};
 
 const detailReducer = (state = initialState, action) => {
   switch (action.type) {
@@ -7,11 +11,15 @@ const detailReducer = (state = initialState, action) => {
         ...state,
         game: action.payload.game,
         screen: action.payload.screen,
+        isLoading: false,
       };
-    default:
+    case "LOADING_DETAIL":
       return {
         ...state,
+        isLoading: true,
       };
+    default:
+      return { ...state };
   }
 };
 
